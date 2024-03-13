0b652b4186dbb8bb399bbae5bcbb9b2ada4b26fa
IraKaidanovich <startel2580startel@gmail.com>
2024-03-13 17:00:57 +0100

    second commit

---
+++ my-app/src/redux/store.js
@@ -0,0 +1,11 @@
+export default {
+  getState: () => console.log('test'),
+  subscribe: () => console.log('subscripbe')
+};
+
+// import { createStore } from 'redux';
+// import rootReducer from './reducers'; 
+
+// const store = createStore(rootReducer);
+
+// export default store;
