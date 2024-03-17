944264b29c62c4e29de4305f2d3084316e9af6eb
IraKaidanovich <startel2580startel@gmail.com>
2024-03-17 16:10:08 +0100

    updates

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore} from 'redux'
 import rootReducer from './reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer({ trace: true }));
 export default store;
 
